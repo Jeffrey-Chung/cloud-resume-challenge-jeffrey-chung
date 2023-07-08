4b1955684fa1ef1a159130175cd04cf434f24dd6
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-07-08 19:45:08 +1000

    Comment out assume role block

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-southeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::663790350014:role/GitHubAction-AssumeRoleWithAction"
-    session_name = "tf-aws-jchung-cloud-resume-challenge"
-    external_id  = "Jeffrey-Chung@github"
-  }
+  #assume_role {
+  #  role_arn     = "arn:aws:iam::663790350014:role/GitHubAction-AssumeRoleWithAction"
+  #  session_name = "tf-aws-jchung-cloud-resume-challenge"
+  #  external_id  = "Jeffrey-Chung@github"
+  #}
 }
 
 terraform {
