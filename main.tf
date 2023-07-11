f6d12d547d27fa31a0cc468b1898f4482a5f29a7
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-07-11 16:23:55 +1000

    Removed bucket policy again

--- main.tf
+++ main.tf
@@ -30,28 +30,6 @@ resource "aws_s3_bucket_acl" "jchung_s3_bucket_acl" {
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "jchung_s3_bucket_policy" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.jchung_s3_bucket_ownership_controls,
-    aws_s3_bucket_public_access_block.jchung_s3_bucket_bucket_public_access_block,
-  ]
-  bucket = aws_s3_bucket.jchung_s3_bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": ["s3:GetObject"],
-            "Resource": ["arn:aws:s3:::tf-aws-jchung-cloud-resume-challenge-bucket/*"]
-        }
-    ]
-}
-EOF
-}
-
 
 resource "aws_s3_object" "html_s3_object" {
   bucket = aws_s3_bucket.jchung_s3_bucket.id
