a024ad48638d51be0caf71bd8ecedd6372b8afa2
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-07-18 16:41:23 +1000

    change permissions on public access block

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ resource "aws_s3_bucket_ownership_controls" "jchung_s3_bucket_ownership_controls
 
 resource "aws_s3_bucket_public_access_block" "jchung_s3_bucket_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.jchung_s3_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "jchung_s3_bucket_acl" {
