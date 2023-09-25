ea7a8e811c5fc1074c3a32751166e1bc738107a8
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-25 14:43:06 +1000

    Hardcode API for test

--- js/counter.js
+++ js/counter.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const counter = document.getElementsByClassName("counter-number");
 async function updateCounter() {
-	let response = fetch(process.env.LAMBDA_URL);
+	let response = fetch("https://3jdv3fjamvarshhthnvpfgg2ii0wnoun.lambda-url.ap-southeast-2.on.aws/");
 	let data = await response.json();
 	counter.innerHTML = `Views: ${data}`;
 }
