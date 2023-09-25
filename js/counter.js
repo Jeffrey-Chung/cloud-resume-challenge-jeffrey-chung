21daac2b9324fb9d7da0c7a45bf65d8d52da8b12
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-25 14:05:06 +1000

    Add console log for API

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const counter = document.getElementsByClassName("counter-number");
-console.log(process.env.LAMBDA_URL)
 async function updateCounter() {
 	let response = fetch(process.env.LAMBDA_URL);
 	let data = await response.json();
