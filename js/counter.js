f7dd71099ab900d8f489a2bcd66b29f3af0fc247
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-26 11:52:16 +1000

    Update Fetch API function with try catch statement

--- js/counter.js
+++ js/counter.js
@@ -1,8 +1,17 @@
 const counter = document.getElementById("counter-number");
 async function updateCounter() {
-	let response = fetch("https://i3tiofgzuvahp7vhqovoxt4gvu0sibep.lambda-url.ap-southeast-2.on.aws/");
-	let data = await response.json();
-	counter.innerHTML = `Views: ${data}`;
+    try {
+        const response = await fetch('https://i3tiofgzuvahp7vhqovoxt4gvu0sibep.lambda-url.ap-southeast-2.on.aws/');
+    
+        if (!response.ok) {
+          throw new Error(`Error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        counter.innerHTML = `Views: ${data}`;
+    }
+    catch (err) {
+        console.log(err);
+      }
 }
-updateCounter();
+await updateCounter();
 
