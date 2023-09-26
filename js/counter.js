56525977e3f3c3227ed1dc20247b362927edcb91
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-26 11:04:08 +1000

    Update Lambda Function URL

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 const counter = document.getElementById("counter-number");
-fetch("")
+fetch("https://u2iduptl7jfxpo5ny4u637g4pm0iqytg.lambda-url.ap-southeast-2.on.aws/")
     .then((response) => JSON.parse(response.json()))
     .then((data) => {
          counter.innerHTML = `Views: ${data}`
