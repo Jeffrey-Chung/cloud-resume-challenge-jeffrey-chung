386681ccd80eed481b924be32b80b02cff10784b
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-26 11:41:06 +1000

    Update API

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 const counter = document.getElementById("counter-number");
 async function updateCounter() {
-	let response = fetch("");
+	let response = fetch("https://i3tiofgzuvahp7vhqovoxt4gvu0sibep.lambda-url.ap-southeast-2.on.aws/");
 	let data = await response.json();
 	counter.innerHTML = `Views: ${data}`;
 }
