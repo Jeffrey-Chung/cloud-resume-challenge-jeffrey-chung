cfe9cfcab457ca3e9529457d9b854bb0ba71eac3
Jeffrey-Chung <Cheuk.Chung@uts.edu.au>
2023-09-26 10:32:55 +1000

    Add API URL

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 const counter = document.getElementById("counter-number");
-fetch("")
+fetch("https://tha2jdndljtvktr6tj3253nnrm0rflxa.lambda-url.ap-southeast-2.on.aws/")
     .then((response) => response.json())
     .then((data) => {
          counter.innerHTML = `Views: ${data}`
