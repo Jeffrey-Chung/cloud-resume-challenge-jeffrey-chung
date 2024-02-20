c28a18ba775d3ba6d131a72358b6b85ebe0b4ae8
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-02-20 16:44:01 +1100

    change acl permissions back to true

--- modules/frontend/s3.tf
+++ modules/frontend/s3.tf
@@ -37,9 +37,9 @@ resource "aws_s3_bucket_ownership_controls" "jchung_s3_bucket_ownership_controls
 
 resource "aws_s3_bucket_public_access_block" "jchung_s3_bucket_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.jchung_s3_bucket.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -86,9 +86,9 @@ resource "aws_s3_bucket_ownership_controls" "jchung_log_bucket_ownership_control
 
 resource "aws_s3_bucket_public_access_block" "jchung_logging_bucket_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.jchung_logging_bucket.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
