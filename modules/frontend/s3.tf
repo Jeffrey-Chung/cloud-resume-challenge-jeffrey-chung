aacc684065d0a5959eb0d0f84737cb21c50c5907
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-02-20 16:37:20 +1100

    change access block to allow acls

--- modules/frontend/s3.tf
+++ modules/frontend/s3.tf
@@ -37,9 +37,9 @@ resource "aws_s3_bucket_ownership_controls" "jchung_s3_bucket_ownership_controls
 
 resource "aws_s3_bucket_public_access_block" "jchung_s3_bucket_bucket_public_access_block" {
   bucket                  = aws_s3_bucket.jchung_s3_bucket.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = true
 }
 
